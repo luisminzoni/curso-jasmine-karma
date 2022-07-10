6d7b6406758dc506b43d5ce1e17923760a8fad94
Luis Minzoni <luis.minzoni@gmail.com>
2022-07-10 19:35:06 -0300

    02. Lapidando nossos testes

--- ng-test1-c69b5329f9e845fca231c22e137dd37aa27b8aec/src/app/shared/services/unique-id/unique-id.service.ts
+++ ng-test1-c69b5329f9e845fca231c22e137dd37aa27b8aec/src/app/shared/services/unique-id/unique-id.service.ts
@@ -6,8 +6,10 @@ import { v4 as uuidv4 } from 'uuid'
 export class UniqueIdService {
   private numberOfGeneratedIds = 0;
 
+  private validId = /^[A-Za-z/]+[\w-:.]*$/;
+
   public generateUniqueIdWithPrefix(prefix: string): string {
-    if (!prefix) {
+    if (!prefix || !this.validId.test(prefix)) {
       throw Error('Prefix can not be empty');
     }
     const uniqueId = this.generateUniqueId()
@@ -20,6 +22,6 @@ export class UniqueIdService {
   }
 
   private generateUniqueId(): string {
-    return uuidv4;
+    return uuidv4();
   }
 }
